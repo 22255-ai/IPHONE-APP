9fa0fe919cb04d78b441e74b7248ee0fe8c32b59
22255-ai <22255@burnside.school.nz>
2026-08-17 13:53:46 +1200

    modified

--- app.py
+++ app.py
@@ -25,31 +25,30 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+@app.route('/iphone/<int:iphone_id>')
+def iphone(iphone_id):
+    #iphone details page
+    sql = """SELECT iphone.id, iphone.model, cpu.name, iphone.front_camera
+             FROM iphone JOIN cpu ON cpu.id=iphone.cpu_id
+             JOIN phone_ram ON phone_ram.phone_id=iphone.id
+             JOIN ram ON ram.id=phone_ram.ram_id
+             WHERE iphone.id = ?"""
+
+    results = query_db(sql, [iphone_id], one=True)
+    print(results)
+    return render_template("iphone.html", results=results)
+
 @app.route('/')
-def home ():
-    #home page 
-    sql = """SELECT i.id, i.model,i.image_url, r.size AS ram_gb
-            FROM iphone i 
-            JOIN phone_ram pr ON i.id = pr.phone_id
-            JOIN ram r ON pr.ram_id = r.id"""
- 
+def home():
+    #home page
+    sql = """SELECT i.id, i.model, i.image_url, r.size AS ram_gb
+             FROM iphone i
+             JOIN phone_ram pr ON i.id = pr.phone_id
+             JOIN ram r ON pr.ram_id = r.id"""
+
     results = query_db(sql)
     print(results)
-    return render_template("home.html",results=results)
-
-@app.route('/iphone/<int:iphone_id>')
-def iphone (iphone_id):
-    #iphone details page
-    sql =    """SELECT iphone.id, iphone.model, cpu.name, iphone.front_camera, iphone.rear_camera, iphone.image_url
-                FROM iphone JOIN cpu ON cpu.id=iphone.cpu_id
-                JOIN phone_ram ON phone_ram.phone_id=iphone.id
-                JOIN ram ON ram.id=phone_ram.ram_id
-                WHERE iphone.id = ?"""
-    
-    
-    results = query_db(sql,[iphone_id],one=True)
-    print(results)
-    return render_template("iphone.html",results=results)
+    return render_template("home.html", results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
